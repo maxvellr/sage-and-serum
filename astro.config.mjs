bb8f5fbedbf55ec06ca121a76092068661ff9f32
Maxwell Ruhr <maxwellruhr@Administrators-MacBook-Air.local>
2026-04-05 13:59:27 -0700

    updating tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 import { defineConfig } from 'astro/config'
-import tailwind from '@astrojs/tailwind'
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  integrations: [tailwind()],
   site: 'https://maxvellr.github.io',
   base: '/sage-and-serum',
+  vite: {
+    plugins: [tailwindcss()],
+  },
 })
